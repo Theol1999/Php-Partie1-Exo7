2abba3e2b6453ca604b12c6f5e89dfb078ee4be9
theoL <theo1007@outlook.fr>
2019-03-13 15:15:51 +0100

    Ajout de l'exo 7

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$lastname = "Theo";
+$firstname = "Levert";
+$age = 19;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        echo "Bonjour $lastname $firstname tu as $age ans";
+      ?>
+    </p>
+  </body>
+</html>
